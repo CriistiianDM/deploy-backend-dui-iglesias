313fe71d683dcaec6259bf9c6bd1c6e1d68c5d8b
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-07-13 22:05:28 -0500

    camvisoss

--- index/bd.js
+++ index/bd.js
@@ -2,7 +2,7 @@ const {Pool} = require('pg');
 const {db} = require('./config');
 
 const database = new Pool({
-    connectionString: `postgres://ykrzhhgvjmhzlx:6d4276eac575c62cb1e5f684e4606eec4a0fac83ae104d285e7285203e8567f4@ec2-23-23-151-191.compute-1.amazonaws.com:5432/dg57o3kgp8v9m`,
+    connectionString: 'postgres://ykrzhhgvjmhzlx:6d4276eac575c62cb1e5f684e4606eec4a0fac83ae104d285e7285203e8567f4@ec2-23-23-151-191.compute-1.amazonaws.com:5432/dg57o3kgp8v9m',
     ssl: {
         rejectUnauthorized: false
     }
