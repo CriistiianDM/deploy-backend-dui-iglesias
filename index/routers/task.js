e0b46abd79fa92cda099aeb720eef18f61a61f02
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-07-22 15:18:59 -0500

    intento #4

--- index/routers/task.js
+++ index/routers/task.js
@@ -1,15 +1,24 @@
-const { Router , application } = require('express');
-const { petitions_get , petitions_get_login , petitions_get_cargo_vigigentes,
-        petitions_put_periodo , petitions_get_user , petitions_get_all_user_active, 
-        petitions_get_user_exist, petitions_get_email_exist , petitions_get_all_country, 
-        petitions_post_user, petitions_get_info_user , petitions_post_file} = require('../controls/controls');
+const { Router, application } = require('express');
+const { petitions_get, petitions_get_login, petitions_get_cargo_vigigentes,
+    petitions_put_periodo, petitions_get_user, petitions_get_all_user_active,
+    petitions_get_user_exist, petitions_get_email_exist, petitions_get_all_country,
+    petitions_post_user, petitions_get_info_user, petitions_post_file } = require('../controls/controls');
 
-const router = Router();
 const multer = require('multer');
+const mimeTypes = require('mimeTypes');
+const router = Router();
 
-const upload = multer({ 
-    dest: '../archivos/',
-   });
+
+const storage = multer.diskStorage({
+    destination: '../archivos/',
+    filename: (req, file, cb) => {
+        cb('', Data.now() + file.originalname + '.' + mimeTypes.extension(file.mimetype));
+    }
+});
+
+const upload = multer({
+    storage: storage
+});
 
 
 //router.get('/', petitions_get);
@@ -33,7 +42,7 @@ router.post('/zincrp', petitions_post_user);
 //la routa se llamara zadtus y sera un get para traer la informacion de un usuario
 router.get('/zadtus/:doc', petitions_get_info_user);
 //la routa se lamara zfiles y sera un post para guardar archivos
-router.post('/zfiles', upload.single('file_img') , petitions_post_file);
+router.post('/zfiles', upload.single('file_img'), petitions_post_file);
 //la ruta se llamara zuppt y tendra un parametro que sera el id del periodo para actualizar el periodo
 router.put('/zuppt/:id', petitions_put_periodo);
 
