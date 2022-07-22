165e2f454dec7d2734b2831cae8bdb47023ed971
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-07-22 17:29:24 -0500

    cambios ahahah

--- index/routers/task.js
+++ index/routers/task.js
@@ -2,7 +2,7 @@ const { Router, application } = require('express');
 const { petitions_get, petitions_get_login, petitions_get_cargo_vigigentes,
     petitions_put_periodo, petitions_get_user, petitions_get_all_user_active,
     petitions_get_user_exist, petitions_get_email_exist, petitions_get_all_country,
-    petitions_post_user, petitions_get_info_user, petitions_post_file } = require('../controls/controls');
+    petitions_post_user, petitions_get_info_user, petitions_post_file, petitions_post_group } = require('../controls/controls');
 
 const multer = require('multer');
 const mimeTypes = require('mime-types');
@@ -47,7 +47,7 @@ router.get('/zadtus/:doc', petitions_get_info_user);
 //la routa se lamara zfiles y sera un post para guardar archivos
 router.post('/zfiles', upload.single('file_img'), petitions_post_file);
 //la routa se llamara zcrgppipe y sera un post para crear un grupo 
-router.post('/zcrgppipe', petitions_put_periodo);
+router.post('/zcrgppipe', petitions_post_group);
 //la ruta se llamara zuppt y tendra un parametro que sera el id del periodo para actualizar el periodo
 router.put('/zuppt/:id', petitions_put_periodo);
 
