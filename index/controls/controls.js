1d8ac404e8527346527c3db47bccf7309d05088a
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-07-24 16:22:45 -0500

    mirando bug1

--- index/controls/controls.js
+++ index/controls/controls.js
@@ -128,6 +128,7 @@ const petitions_get_cargo_vigigentes = async (req, res) => {
 
                 if (index === (index_answer2 - 1)) {
                     categoria_vigente = `${categoria_vigente.substring(0, categoria_vigente.length - 1)}`;
+                    console.log(categoria_vigente, 'final');
                     res.json(categoria_vigente);
                 }
                 console.log(categoria_vigente, 'categoria_vigente');
