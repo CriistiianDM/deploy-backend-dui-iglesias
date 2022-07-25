c78e0395231f14d04534790e1ede7684046c671b
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-07-24 19:49:24 -0500

    ya

--- index/controls/controls.js
+++ index/controls/controls.js
@@ -464,6 +464,7 @@ const petitions_post_group = async (req, res) => {
         const answer = await pool.query(`INSERT INTO groups_eclesial  (id, name, description, status , url_img , logical_erase) VALUES (nextval('groups_seq'), $1, $2, 'activo', $3, false)`, [name, description, application['img']]);
         const comsult_id_group = await pool.query(`SELECT id FROM groups_eclesial WHERE name = $1 AND logical_erase = false`, [name]);
         const id_group = comsult_id_group.rows[0].id;
+        console.log(id_group,'sebas mirala')
         const answer2 = await pool.query(`INSERT INTO person_group (id, person_id, groups_id ,position_id ,status,logical_erase) VALUES (nextval('person_group_seq'), $1, $2, 4 ,'A', false)`, [id_person, id_group]);
 
 
@@ -502,12 +503,13 @@ const petitions_post_position = async (req, res) => {
 }
 
 
+
+
 /**
   *  @author : Juan Felipe Osorio Zapata <juan.felipe.osorio@correounivalle.edu.co>
   *  @decs  : get para obtener los jovenes lideres que no pertenezcan a un grupo
   * 
 */
-
 const petitions_get_jovenes_lideres = async (req, res) => {
 
     try {
