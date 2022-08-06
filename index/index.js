6e95ba123e96ec8b4bbb3799e239ec9d11619fcd
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-08-05 22:33:00 -0500

    test

--- index/index.js
+++ index/index.js
@@ -23,7 +23,7 @@ const verificar = async (req, res, next) => {
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
-app.use(verificar);
+app.use(verificar());
 app.use(taskRouter);
 
 
