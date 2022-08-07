2c12bda57811509bf31866ac840a2f25506a3a01
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-08-07 14:09:49 -0500

    ya quedaron acomodados los middleware en un modulo aparte, solo queda verificar

--- index/index.js
+++ index/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const taskRouter = require('./routers/task');
-const { only_petitions_fronted } = require('./middleware/middleware');
+const { only_petitions_fronted,
+        verificar_post_cr_user  } = require('./middleware/middleware');
 const { PORT } = process.env;
 
 
@@ -13,28 +14,6 @@ const app = express();
 //settings
 app.set('port',  4500);
 
-/*
-   TODO: mañana hacer un archivo pa los middlewares y empezar por esta parte
-   ? primer avance de seguridad para las peticiones y backedend
-   ! no olvidar hacer una funcion de encriptacion para la clave de acceso para las peticiones y esta cambiaria automaticamente cada dia
-
-const only_petitions_fronted = async (req, res, next) => {
-    console.log('holi verificar 12', (req.headers));
-
-    if ((req.headers).origin === 'https://iglesia-pentacostal-colombia.vercel.app') {
-        next();
-    }
-    else {
-        res.status(401).send('acceso denegado');
-    }
-   
-}*/
-
-//verfificar como llegan los datos
-const verificar = async (req, res, next) => {
-    console.log( await req.body,await req.params,'holi verificar');
-    next();
-}
 
 
 //middleware
@@ -42,7 +21,7 @@ app.use(express.json());
 app.use(morgan('short'));
 app.use(cors());
 app.use(only_petitions_fronted);
-app.all('/zincrp', verificar);
+app.all('/zincrp', verificar_post_cr_user);
 app.use(taskRouter);
 
 
--- index/middleware/middleware.js
+++ index/middleware/middleware.js
@@ -1,6 +1,7 @@
 
 /**
   *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  !no olvidar hacer la funcion de encriptacion para validar el token de seguridad
   *  @decs  : middelware para validar que solo se puedan ingresar peticiones desde el frontend
 */
 const only_petitions_fronted = async (req, res, next) => {
@@ -16,8 +17,18 @@ const only_petitions_fronted = async (req, res, next) => {
 }
 
 
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : middelware para validar que los datos de la peticion sean correctos
+*/
+const verificar_post_cr_user = async (req, res, next) => {
+    console.log( await req.body,await req.params,'holi verificar');
+    next();
+}
+
 
 //exportar el middelware
 module.exports = {
-    only_petitions_fronted
+    only_petitions_fronted,
+    verificar_post_cr_user
 }
