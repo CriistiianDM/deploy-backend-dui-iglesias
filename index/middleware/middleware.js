54104f3d18b0799b3f0f140b9e07be45ddf4f56d
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-08-07 14:29:48 -0500

    test de middileware

--- index/middleware/middleware.js
+++ index/middleware/middleware.js
@@ -22,9 +22,47 @@ const only_petitions_fronted = async (req, res, next) => {
   *  @decs  : middelware para validar que los datos de la peticion sean correctos
 */
 const verificar_post_cr_user = async (req, res, next) => {
-    console.log( await req.body,await req.params,'holi verificar');
+    console.log( req.body, req.params,'holi verificar');
+    const state = getNameState();
+    
+    (state).map( (item) => {
+        console.log(item,'trending toping');
+    });
+    
     next();
 }
+
+
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : retornar el nombres de los parametros no nulos
+*/
+function getNameState() {
+
+    const state = [
+        {'data': 'doc'},
+        {'data': 'first_name'},
+        {'data': 'first_last_name'},
+        {'data': 'doc_type'},
+        {'data': 'birth_date'},
+        {'data': 'email'},
+        {'data': 'phone_1'},
+        {'data': 'gender'},
+        {'data': 'address'},
+        {'data':  'place_birth'},
+        {'data': 'baptism_date'},
+        {'data': 'baptism_place_id'},
+        {'data': 'holy_spirit_date'},
+        {'data': 'date_init_church'}
+    ]
+
+    return state;
+
+}
+
+
+
 
 
 //exportar el middelware
