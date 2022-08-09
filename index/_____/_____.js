666852b98842a2a121e0db008372714b709bcfd4
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-08-08 23:02:57 -0500

    prueba

--- index/_____/_____.js
+++ index/_____/_____.js
@@ -1,1 +1,166 @@
-var _0x9caf=["length","substring","|","split","","map","E|!|1|d|5|d|s|6|7|9|s|5|r|6","4|k|5|d|5|6|s|5|7|h|8|t|r|e","5|d|j|f|9|8|s|e|7|h|8|f|6|e","6|d|k|f|9|8|s|e|7|h|8|5|6|3","7|c|k|f|9|8|s|e|7|h|8|5|6|4","8|g|k|f|9|8|s|e|7|h|8|f|6|h","9|c|k|f|9|8|s|e|7|h|8|j|6|f","0|h|7|9|9|8|t|t|7|h|8|h|6|7","1|j|k|f|9|8|s|t|7|h|8|m|6|f","2|j|k|f|9|8|r|e|7|h|8|n|6|t","3|j|k|f|9|8|e|e|7|h|8|z|6|6","4|j|k|f|9|4|s|e|7|h|8|x|6|f","5|j|k|f|9|5|s|e|7|h|8|a|6|e","6|j|k|f|7|8|s|y|u|c|a|x|6|2","7|5|k|f|9|r|s|r|d|h|8|g|6|2","8|6|k|f|9|8|s|e|7|h|8|c|6|1","9|7|k|f|9|8|s|e|7|h|8|f|6|q","0|8|k|f|9|8|s|e|7|h|8|4|6|a","1|9|k|f|9|8|s|e|7|h|8|6|6|a","2|7|k|f|9|8|s|e|7|h|8|7|6|s","3|5|t|l|t|8|s|e|7|h|8|8|6|c","4|j|k|f|t|r|d|e|7|h|8|0|6|l","5|j|k|f|9|8|s|e|7|h|8|f|6|f","6|j|k|f|9|8|s|e|7|y|8|h|6|s","7|j|k|f|9|8|s|e|7|u|8|k|6|a","8|j|k|f|9|8|s|e|7|u|8|3|6|a","9|j|k|f|9|8|s|e|7|s|8|f|6|d","0|j|k|f|9|8|s|e|7|q|8|g|6|7","1|j|k|f|9|8|s|e|7|q|8|6|6|5","2|j|k|f|9|8|s|e|7|f|8|7|6|t","3|j|k|f|9|8|s|e|7|h|8|f|6|i","%uA=","E$12","F$#2","M$!3","A$&4","M*(5","Jp}6","J%_7","Aio8","S$#9","O?<1",",>N1","D$#2","E%%3","P$$4","~@L5","@@16","S@17","/7*8",")619","D}?0","??21","t}<2","O$#$","L!~@","@_>{","*-?Z","_+?A",':"AW',"$/&5","E#30","c*c*","#408","F$#3","M$#4","A$#5","0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","exports"];const generateToken=a=>{let b=a[_0x9caf[1]](a[_0x9caf[0]]-1),c=token_month[b][_0x9caf[3]](_0x9caf[2]),d=_0x9caf[4];return c[_0x9caf[5]](a=>{d+=encrypt_token[a]}),(d+=b)===a},token_month={"1":_0x9caf[6],"2":_0x9caf[7],"3":_0x9caf[8],"4":_0x9caf[9],"5":_0x9caf[10],"6":_0x9caf[11],"7":_0x9caf[12],"8":_0x9caf[13],"9":_0x9caf[14],"10":_0x9caf[15],"11":_0x9caf[16],"12":_0x9caf[17],"13":_0x9caf[18],"14":_0x9caf[19],"15":_0x9caf[20],"16":_0x9caf[21],"17":_0x9caf[22],"18":_0x9caf[23],"19":_0x9caf[24],"20":_0x9caf[25],"21":_0x9caf[26],"22":_0x9caf[27],"23":_0x9caf[28],"24":_0x9caf[29],"25":_0x9caf[30],"26":_0x9caf[31],"27":_0x9caf[32],"28":_0x9caf[33],"29":_0x9caf[34],"30":_0x9caf[35],"31":_0x9caf[36]},encrypt_token={"0":_0x9caf[37],"1":_0x9caf[38],"2":_0x9caf[39],"3":_0x9caf[40],"4":_0x9caf[41],"5":_0x9caf[42],"6":_0x9caf[43],"7":_0x9caf[44],"8":_0x9caf[45],"9":_0x9caf[46],a:_0x9caf[47],b:_0x9caf[48],c:_0x9caf[49],d:_0x9caf[50],e:_0x9caf[51],f:_0x9caf[52],g:_0x9caf[53],h:_0x9caf[54],i:_0x9caf[55],j:_0x9caf[56],k:_0x9caf[57],l:_0x9caf[58],m:_0x9caf[59],n:_0x9caf[60],o:_0x9caf[61],p:_0x9caf[62],q:_0x9caf[63],r:_0x9caf[64],s:_0x9caf[65],t:_0x9caf[66],u:_0x9caf[67],v:_0x9caf[68],w:_0x9caf[69],x:_0x9caf[70],y:_0x9caf[71],z:_0x9caf[72]},decrypt_token={"%uA=":_0x9caf[73],"E$12":_0x9caf[74],"F$#2":_0x9caf[75],"M$!3":_0x9caf[76],"A$&4":_0x9caf[77],"M*(5":_0x9caf[78],"Jp}6":_0x9caf[79],"J%_7":_0x9caf[80],Aio8:_0x9caf[81],"S$#9":_0x9caf[82],"O?<1":_0x9caf[83],",>N1":_0x9caf[84],"D$#2":_0x9caf[85],"E%%3":_0x9caf[86],"P$$4":_0x9caf[87],"~@L5":_0x9caf[88],"@@16":_0x9caf[89],"S@17":_0x9caf[90],"/7*8":_0x9caf[91],")619":_0x9caf[92],"D}?0":_0x9caf[93],"??21":_0x9caf[94],"t}<2":_0x9caf[95],"O$#$":_0x9caf[96],"L!~@":_0x9caf[97],"@_>{":_0x9caf[98],"*-?Z":_0x9caf[99],"_+?A":_0x9caf[100],':":AW':_0x9caf[101],"$/&5":_0x9caf[102],"E#30":_0x9caf[103],"c*c*":_0x9caf[104],"#408":_0x9caf[105],"F$#3":_0x9caf[106],"M$#4":_0x9caf[107],"A$#5":_0x9caf[108]};module[_0x9caf[109]]={generateToken}
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  TODO: toca dejar lac claves predeterminadas por cuestiones de tiempo
+  *  @decs  : funcion que genera un token de seguridad para las peticiones ajax
+*/
+const generateToken = (token_param) => {
+
+    if (token_param.length > 0) {
+    // sacar la ultima letra del token
+    let day = token_param.substring(token_param.length - 1);
+    const password_token = ((token_month[day]).split('|'));
+    let token = '';
+
+    (password_token).map(
+        (item) => {
+            token += encrypt_token[item];
+        }
+    )
+
+    token += day;
+    
+    if (token === token_param) {
+        return true;
+    }
+
+    return false;
+
+    }
+
+    return false;
+
+}
+
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : dependiendo del dia esocgera una clave predeterminada para el token
+*/
+const token_month = {
+    '1': 'E|!|1|d|5|d|s|6|7|9|s|5|r|6',
+    '2': '4|k|5|d|5|6|s|5|7|h|8|t|r|e',
+    '3': '5|d|j|f|9|8|s|e|7|h|8|f|6|e',
+    '4': '6|d|k|f|9|8|s|e|7|h|8|5|6|3',
+    '5': '7|c|k|f|9|8|s|e|7|h|8|5|6|4',
+    '6': '8|g|k|f|9|8|s|e|7|h|8|f|6|h',
+    '7': '9|c|k|f|9|8|s|e|7|h|8|j|6|f',
+    '8': '0|h|7|9|9|8|t|t|7|h|8|h|6|7',
+    '9': '1|j|k|f|9|8|s|t|7|h|8|m|6|f',
+    '10': '2|j|k|f|9|8|r|e|7|h|8|n|6|t',
+    '11': '3|j|k|f|9|8|e|e|7|h|8|z|6|6',
+    '12': '4|j|k|f|9|4|s|e|7|h|8|x|6|f',
+    '13': '5|j|k|f|9|5|s|e|7|h|8|a|6|e',
+    '14': '6|j|k|f|7|8|s|y|u|c|a|x|6|2',
+    '15': '7|5|k|f|9|r|s|r|d|h|8|g|6|2',
+    '16': '8|6|k|f|9|8|s|e|7|h|8|c|6|1',
+    '17': '9|7|k|f|9|8|s|e|7|h|8|f|6|q',
+    '18': '0|8|k|f|9|8|s|e|7|h|8|4|6|a',
+    '19': '1|9|k|f|9|8|s|e|7|h|8|6|6|a',
+    '20': '2|7|k|f|9|8|s|e|7|h|8|7|6|s',
+    '21': '3|5|t|l|t|8|s|e|7|h|8|8|6|c',
+    '22': '4|j|k|f|t|r|d|e|7|h|8|0|6|l',
+    '23': '5|j|k|f|9|8|s|e|7|h|8|f|6|f',
+    '24': '6|j|k|f|9|8|s|e|7|y|8|h|6|s',
+    '25': '7|j|k|f|9|8|s|e|7|u|8|k|6|a',
+    '26': '8|j|k|f|9|8|s|e|7|u|8|3|6|a',
+    '27': '9|j|k|f|9|8|s|e|7|s|8|f|6|d',
+    '28': '0|j|k|f|9|8|s|e|7|q|8|g|6|7',
+    '29': '1|j|k|f|9|8|s|e|7|q|8|6|6|5',
+    '30': '2|j|k|f|9|8|s|e|7|f|8|7|6|t',
+    '31': '3|j|k|f|9|8|s|e|7|h|8|f|6|i'
+}
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : encrytar el token de forma predeterminada
+*/
+const encrypt_token = {
+    '0': '%uA=',
+    '1': 'E$12',
+    '2': 'F$#2',
+    '3': 'M$!3',
+    '4': 'A$&4',
+    '5': 'M*(5',
+    '6': 'Jp}6',
+    '7': 'J%_7',
+    '8': 'Aio8',
+    '9': 'S$#9',
+    'a': 'O?<1',
+    'b': ',>N1',
+    'c': 'D$#2',
+    'd': 'E%%3',
+    'e': 'P$$4',
+    'f': '~@L5',
+    'g': '@@16',
+    'h': 'S@17',
+    'i': '/7*8',
+    'j': ')619',
+    'k': 'D}?0',
+    'l': '??21',
+    'm': 't}<2',
+    'n': 'O$#$',
+    'o': 'L!~@',
+    'p': '@_>{',
+    'q': '*-?Z',
+    'r': '_+?A',
+    's': ':"AW',
+    't': '$/&5',
+    'u': 'E#30',
+    'v': 'c*c*',
+    'w': '#408',
+    'x': 'F$#3',
+    'y': 'M$#4',
+    'z': 'A$#5'
+}
+
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : desincriptar el token de forma predeterminada
+*/
+const decrypt_token = {
+    '%uA=': '0',
+    'E$12': '1',
+    'F$#2': '2',
+    'M$!3': '3',
+    'A$&4': '4',
+    'M*(5': '5',
+    'Jp}6': '6',
+    'J%_7': '7',
+    'Aio8': '8',
+    'S$#9': '9',
+    'O?<1': 'a',
+    ',>N1': 'b',
+    'D$#2': 'c',
+    'E%%3': 'd',
+    'P$$4': 'e',
+    '~@L5': 'f',
+    '@@16': 'g',
+    'S@17': 'h',
+    '/7*8': 'i',
+    ')619': 'j',
+    'D}?0': 'k',
+    '??21': 'l',
+    't}<2': 'm',
+    'O$#$': 'n',
+    'L!~@': 'o',
+    '@_>{': 'p',
+    '*-?Z': 'q',
+    '_+?A': 'r',
+    ':":AW': 's',
+    '$/&5': 't',
+    'E#30': 'u',
+    'c*c*': 'v',
+    '#408': 'w',
+    'F$#3': 'x',
+    'M$#4': 'y',
+    'A$#5': 'z'
+}
+
+
+//exportar la funcion para generar el token
+
+module.exports = {
+    generateToken
+}
+
