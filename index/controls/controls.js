2c8c9db305bb39741e42357cf37fb6824d065043
Cristian Duvan Machado Mosquera <cristiankm408@gmail.com>
2022-08-11 23:47:13 -0500

    restauracion de master

--- index/controls/controls.js
+++ index/controls/controls.js
@@ -367,6 +367,7 @@ const petitions_post_user = async (req, res) => {
             id_church_now
         } = req.body;
 
+
         //insertar usuario
         const answer1 = await pool.query(`INSERT INTO user_account (id, doc, passwd, logical_erase) VALUES (nextval('user_seq'), $1, $1, false)`, [doc]);
         const consult_1 = await pool.query(`SELECT id FROM user_account WHERE doc = $1 AND logical_erase = false`, [doc]);
@@ -387,8 +388,9 @@ const petitions_post_user = async (req, res) => {
 
         const answer4 = await pool.query(`INSERT INTO person_position (id, name , person_id , position_id, period_id , id_group ,logical_erase) VALUES (nextval('person_position_seq'), 'creyente', $1, 1, 1,NULL, false)`, [id_person]);
 
+        console.log('req.body', answer4);
         //retonar la respuesta
-        res.json(answer4.rows);
+        res.json({ message: 'ok' });
 
 
     } catch (error) {
@@ -431,14 +433,7 @@ const petitions_post_file = async (req, res) => {
 
     try {
 
-        //variables para capturar los parametros
-        // const { id_person, file_name, file_type, file_path } = req.body;
-
-        //insertar usuario
-        //const answer = await pool.query(`INSERT INTO person_file (id, person_id, file_name, file_type, file_path, logical_erase) VALUES (nextval('person_file_seq'), $1, $2, $3, $4, false)`, [id_person, file_name, file_type, file_path]);
-        //console.log('req.body', answer);
-        //retonar la respuesta);
-        res.send('todo bien');
+        res.json({ message: 'ok' });
 
     } catch (error) {
         console.log(error, 'error');
@@ -458,13 +453,18 @@ const petitions_post_group = async (req, res) => {
     try {
 
         //variables para capturar los parametros
-        const { name, description } = req.body;
+        const { name, description,id_person } = req.body;
 
         //insertar usuario
         const answer = await pool.query(`INSERT INTO groups_eclesial  (id, name, description, status , url_img , logical_erase) VALUES (nextval('groups_seq'), $1, $2, 'activo', $3, false)`, [name, description, application['img']]);
+        const comsult_id_group = await pool.query(`SELECT id FROM groups_eclesial WHERE name = $1 AND logical_erase = false`, [name]);
+        const id_group = comsult_id_group.rows[0].id;
+        const answer2 = await pool.query(`INSERT INTO person_group (id, person_id, groups_id ,position_id ,status,logical_erase) VALUES (nextval('person_group_seq'), $1, $2, 4 ,'A', false)`, [id_person, id_group]);
+
+
         console.log('salida', answer);
         //retonar la respuesta
-        res.json(answer.rows);
+        res.json({ message: 'ok' });
 
     } catch (error) {
         console.log(error, 'error');
@@ -488,7 +488,7 @@ const petitions_post_position = async (req, res) => {
         const id_person = answer.rows[0].id;
         console.log('id_person', id_person);
         const consult_1 = await pool.query(`INSERT INTO person_position (id, name , person_id , position_id, period_id , id_group ,logical_erase) VALUES (nextval('person_position_seq'), $2, $1 , $3 , 1,NULL, false)`, [id_person,name_cargo,id_cargo]);
-        res.json(consult_1.rows);
+        res.json({ message: 'ok' });
 
     } catch (error) {
         console.log(error, 'error');
@@ -497,18 +497,17 @@ const petitions_post_position = async (req, res) => {
 }
 
 
+
+
 /**
   *  @author : Juan Felipe Osorio Zapata <juan.felipe.osorio@correounivalle.edu.co>
   *  @decs  : get para obtener los jovenes lideres que no pertenezcan a un grupo
   * 
 */
-
 const petitions_get_jovenes_lideres = async (req, res) => {
 
     try {
 
-        //variables para capturar los parametros
-        const { id_group } = req.params;
         //consulta
         const answer = await pool.query(`SELECT  first_name , first_last_name , doc,name , t1.id FROM person AS t1
         JOIN person_position AS t2 ON t1.id = t2.person_id 
@@ -524,31 +523,26 @@ const petitions_get_jovenes_lideres = async (req, res) => {
 }
 
 
-/*
- doc: '',
-        doc_from: 'colombia',
-        doc_type: '',
-        first_name: '',
-        second_name: '',
-        first_last_name: '',
-        second_last_name: '',
-        birth_date: '',
-        email: '',
-        phone_1: '',
-        phone_2: '',
-        gender: '',
-        address: '',
-        type_person: 'normal',
-        place_birth: '',
-        baptism_date: '',
-        baptism_place_id: '',
-        holy_spirit_date: '',
-        date_init_church: '',
-        experience_json: '',
-        id_church_now:
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : get para obtener los nombres de los grupos
 */
+const petitions_get_group_exist = async (req, res) => {
+    try {
+   
+        const { name } = req.params;
 
+        //consulta
+        const answer = await pool.query(`SELECT name FROM groups_eclesial WHERE name = $1 AND logical_erase = false`, [name]);
 
+        //retonar la respuesta
+        res.json(answer.rows);
+
+    } catch (error) {
+        console.log(error, 'error');
+    }
+}
 
 module.exports = {
     petitions_get,
@@ -566,7 +560,8 @@ module.exports = {
     petitions_post_group,
     petitions_post_position, 
     petitions_get_jovenes_lideres,
-    petitions_get_cargoFaltantesUser
+    petitions_get_cargoFaltantesUser,
+    petitions_get_group_exist
 
 }
 
---
+++ index/middleware/middleware.js
@@ -0,0 +1,225 @@
+
+const { generateToken } = require('./../_____/_____');
+
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  !no olvidar hacer la funcion de encriptacion para validar el token de seguridad
+  *  @decs  : middelware para validar que solo se puedan ingresar peticiones desde el frontend
+*/
+const only_petitions_fronted = async (req, res, next) => {
+    console.log('holi verificar 12', (req.headers),generateToken((req.headers).authorization),(req.headers).authorization);
+
+    if ((req.headers).origin === 'https://iglesia-pentacostal-colombia.vercel.app' ||
+        (req.headers).origin === 'http://localhost:3000') {
+
+        if (generateToken( ((req.headers).authorization) )) {
+            next();
+        }
+        else {
+            res.status(401).send('acceso denegado');
+        }
+    }
+    else {
+        res.status(401).send('acceso denegado');
+    }
+
+}
+
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : middelware para validar que los datos de la peticion sean correctos
+*/
+const verificar_post_cr_user = async (req, res, next) => {
+   
+    const state = getNameState();
+    let index = 0;
+
+    (state).map((item) => {
+       
+        if (!validateFormate(((req.body)[item.data]),item.type)) {
+           res.json({ message: 'error en el formato de los datos' });
+        }else {
+            index++;
+        }
+    });
+
+    
+    if (index === state.length) {
+        next();
+    }
+}
+
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : middelware para validar que los datos de la peticion crear grupo sean correctos
+  *
+*/
+const verificar_post_cr_group = async (req, res, next) => {
+
+    try {
+        const state = get_name_state();
+        let index = 0;
+        let index_aux = 0;
+    
+        (state).map((item) => {
+           
+            if (!validateFormate(((req.body)[item.data]),item.type) && ((req.body)[item.data]) != '') {
+               res.json({ message: 'error en el formato de los datos' });
+            }
+
+            if (index_aux === 0 && validateFormate(((req.body)[item.data]),item.type)||
+               index_aux === 3 && validateFormate(((req.body)[item.data]),item.type)) {
+                index++;
+            }
+
+            index_aux++;
+        });
+    
+        
+        if (index === (state.length - 2 )) {
+            next();
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : middelware para validar que los campos de la peticion sean correctos para la asignacion de cargo
+  *
+*/
+const verificar_post_cr_cargo = async (req, res, next) => {
+
+    const state = get_name_state_cargo();
+    let index = 0;
+
+    (state).map((item) => {
+       
+        if (!validateFormate(((req.body)[item.data]),item.type)) {
+           res.json({ message: 'error en el formato de los datos' });
+        }else {
+            index++;
+        }
+    });
+
+    
+    if (index === state.length) {
+        next();
+    }
+
+}
+
+
+/*
+  ------- funciones complementarias -------
+*/
+
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : retornar el nombres de los parametros no nulos
+*/
+function getNameState() {
+
+    const state = [
+        { 'data': 'doc', 'type': 0 },
+        { 'data': 'first_name', 'type': 1 },
+        { 'data': 'first_last_name', 'type': 1 },
+        { 'data': 'doc_type', 'type': 2 },
+        { 'data': 'birth_date', 'type': 3 },
+        { 'data': 'email', 'type': 4 },
+        { 'data': 'phone_1', 'type': 9 },
+        { 'data': 'gender', 'type': 5 },
+        { 'data': 'address', 'type': 6 },
+        { 'data': 'place_birth', 'type': 7 },
+        { 'data': 'baptism_date', 'type': 3 },
+        { 'data': 'baptism_place_id', 'type': 7 },
+        { 'data': 'holy_spirit_date', 'type': 3 },
+        { 'data': 'date_init_church', 'type': 3 },
+    ]
+
+    return state;
+
+}
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : Validar formatos
+*/
+function validateFormate(e, type) {
+
+    //expresssion regular 
+    var regex = {
+        '0': /^[0-9]{9,15}$/,
+        '1': /^[a-zA-Z]{3,50}$/,
+        '2': /^[a-zA-Z]{2}$/,
+        '3': /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/,
+        '4': /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+        '5': /^[a-zA-Z]$/,
+        '6': /^[0-9-a-zA-Z\s#\-]{10,255}$/,
+        '7': /^[0-9]{1,5}$/,
+        '8': /^{[0-9-aA-zZ:,"\s{}]*}$/,
+        '9': /^[0-9]{7,15}$/,
+        '10': /^[a-zA-Z\s]{3,50}$/,
+        '11': /^[0-9-aA-zZ\s\/:.]+$/,
+    }
+
+    //validar el formato
+    if ((regex[type]).exec(e) != null) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : retornar el nombre de state a cambiar
+*/
+function get_name_state() {
+
+    const state = [
+      {'data': 'name' , 'type': 10},
+      {'data': 'description' , 'type': 10},
+      {'data': 'img_data' , 'type': 11},
+      {'data': 'id_person', 'type': 7},
+    ]
+  
+    return state;
+  
+  }
+
+
+/**
+  *  @author : cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
+  *  @decs  : retornar el nombre de state a cambiar
+*/
+function get_name_state_cargo() {
+    
+    const state = [
+        {'data': 'doc' , 'type': 0},
+        {'data': 'name_cargo' , 'type': 10},
+        {'data': 'id_cargo', 'type': 7},
+    ]
+    
+    return state;
+    
+}
+
+
+//exportar el middelware
+module.exports = {
+    only_petitions_fronted,
+    verificar_post_cr_user,
+    verificar_post_cr_group,
+    verificar_post_cr_cargo
+}
